5b2d537ac32700b1c9c9b27f12e80c0c17aceb0e
Phuong1009 <iphuongdev1009@gmail.com>
2022-05-13 16:43:47 +0700

    Initial Project - Happy Programming

---
+++ src/main/java/swp391_sum22/happyprogramming/HappyProgrammingApplication.java
@@ -0,0 +1,14 @@
+package swp391_sum22.happyprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyProgrammingApplication.class, args);
+        System.out.println("Hello SWP391");
+    }
+
+}
---
+++ src/test/java/swp391_sum22/happyprogramming/HappyProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package swp391_sum22.happyprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
