3badf773deeeecbe6981cb7e59d1f3fbf7ba14ab
promtheusalpha <hoangnguyen.hn212.hn@gmail.com>
2022-05-18 07:35:06 +0700

    UPDATE index.html style (incomplete)

--- src/main/java/swp391_sum22/happyprogramming/model/User.java
+++ src/main/java/swp391_sum22/happyprogramming/model/User.java
@@ -1,14 +1,19 @@
 package swp391_sum22.happyprogramming.model;
 
 import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Objects;
 
 
 @Entity
 @Table(name = "Users")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,4 +33,17 @@ public class User {
     private Date modified;
     @Column(name = "profile_id")
     private Long profile_id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
