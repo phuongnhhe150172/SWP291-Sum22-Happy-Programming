c3f54bb370e73511da7ec86ce768ac974e4a2f6e
hoang nam <93869967+namhv6626@users.noreply.github.com>
2022-05-20 16:15:54 +0700

    add view and update profile mentor

--- src/main/java/swp/happyprogramming/controllers/mentor/MentorProfileController.java
+++ src/main/java/swp/happyprogramming/controllers/mentor/MentorProfileController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 import swp.happyprogramming.dto.MentorDTO;
 import swp.happyprogramming.services.servicesimpl.MentorService;
@@ -23,6 +24,18 @@ public class MentorProfileController {
             return "profile";
         } catch (NumberFormatException e) {
             return "redirect:/index";
+        }
+    }
+
+    @GetMapping("/mentor/profile/update")
+    public String updateProfileMentor(Model model,@RequestParam(value = "id",required = false) String id){
+        try{
+            long mentorId = Integer.parseInt(id);
+            MentorDTO mentorDTO = mentorService.findMentor(mentorId);
+            model.addAttribute("mentor",mentorDTO);
+            return "mentor/profile/update";
+        }catch (NumberFormatException e){
+            return "redirect:index";
         }
     }
 }
