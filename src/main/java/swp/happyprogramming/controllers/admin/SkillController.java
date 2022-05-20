0f0af4aa5a79848bbd36ae887c6d07793e5cec7c
unknown <ttk180902@gmail.com>
2022-05-20 14:06:29 +0700

    ADD UserSkill relationship and ADD skill controller

--- src/main/java/swp/happyprogramming/controllers/admin/SkillController.java
+++ src/main/java/swp/happyprogramming/controllers/admin/SkillController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import swp.happyprogramming.model.Skill;
 import swp.happyprogramming.services.servicesimpl.SkillService;
 
@@ -16,6 +17,12 @@ public class SkillController {
 
     @GetMapping("/admin/listskill")
     public String getAllSkill(Model model){
+//        Skill skill1 = new Skill();
+//        skill1.setSkillName("Java");
+//        Skill skill2 = new Skill();
+//        skill2.setSkillName("Python");
+//        skillService.save(skill1);
+//        skillService.save(skill2);
         List<Skill> skillList = skillService.getAllSkill();
         model.addAttribute("skillList", skillList);
         return "showskill";
