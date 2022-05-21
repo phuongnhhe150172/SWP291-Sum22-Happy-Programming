aaade30f4f95a7cf167ae463ae259ed98d5360c4
promtheusalpha <hoangnguyen.hn212.hn@gmail.com>
2022-05-21 16:33:23 +0700

    UPDATE html

--- src/main/java/swp/happyprogramming/controllers/HomeController.java
+++ src/main/java/swp/happyprogramming/controllers/HomeController.java
@@ -2,12 +2,6 @@ package swp.happyprogramming.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import swp.happyprogramming.model.Skill;
-import swp.happyprogramming.model.User;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Controller
 public class HomeController {
