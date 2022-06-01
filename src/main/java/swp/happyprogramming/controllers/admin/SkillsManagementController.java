66b1514932e8be61c58438a8eb6f028e8b5c6031
Trung Kien <ttk180902@gmail.com>
2022-06-01 20:28:25 +0700

    UPDATE all-skill.html, create skill

--- src/main/java/swp/happyprogramming/controllers/admin/SkillsManagementController.java
+++ src/main/java/swp/happyprogramming/controllers/admin/SkillsManagementController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import swp.happyprogramming.model.Skill;
+import swp.happyprogramming.services.ISkillService;
 import swp.happyprogramming.services.servicesimpl.SkillService;
 
 import java.util.List;
@@ -15,26 +16,27 @@ import java.util.List;
 @Controller
 public class SkillsManagementController {
     @Autowired
-    private SkillService skillService;
+    private ISkillService skillService;
 
     @GetMapping("/admin/listskill")
     public String getAllSkill(Model model){
-//        Skill skill1 = new Skill();
-//        skill1.setSkillName("Java");
-//        Skill skill2 = new Skill();
-//        skill2.setSkillName("Python");
-//        skillService.save(skill1);
-//        skillService.save(skill2);
         List<Skill> skillList = skillService.getAllSkill();
         model.addAttribute("skillList", skillList);
-        return "showskill";
+        return "admin/all-skills";
     }
+
+    @GetMapping("/admin/createskill")
+    public String createSkill(){
+        return "admin/skillCreate";
+    }
+
+
     @PostMapping("/admin/createskill")
     public @ResponseBody
-    String createSkill(Model model, @RequestParam String skillname){
+    String createSkill(@RequestParam String skillName){
         Skill skill = new Skill();
 
-        skill.setName(skillname);
+        skill.setName(skillName);
         skillService.save(skill);
         return "Saved successfully!";
     }
