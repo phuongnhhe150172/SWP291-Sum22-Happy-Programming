15f4db6cbdfbfc659b54a35dbd8804e0b2de7ff0
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2022-06-02 15:36:53 +0700

    Update happyprogramming.sql

--- sql/happyprogramming.sql
+++ sql/happyprogramming.sql
@@ -42,33 +42,6 @@ LOCK TABLES `address` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `answers`
---
-
-DROP TABLE IF EXISTS `answers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `answers` (
-  `id` int NOT NULL,
-  `question_id` int DEFAULT NULL,
-  `answer` varchar(45) DEFAULT NULL,
-  `is_correct` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `questionidanswer_idx` (`question_id`),
-  CONSTRAINT `questionidanswer` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `answers`
---
-
-LOCK TABLES `answers` WRITE;
-/*!40000 ALTER TABLE `answers` DISABLE KEYS */;
-/*!40000 ALTER TABLE `answers` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `attachments`
 --
 
@@ -380,8 +353,7 @@ CREATE TABLE `posts` (
   `price` float DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
-  `comment_count` int DEFAULT NULL,
-  `like_count` int DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `menteeidpost_idx` (`mentee_id`),
   KEY `methodidpost_idx` (`method_id`),
@@ -422,113 +394,6 @@ LOCK TABLES `province` WRITE;
 /*!40000 ALTER TABLE `province` DISABLE KEYS */;
 INSERT INTO `province` VALUES (1,'Thành phố Hà Nội','Thành phố Trung ương'),(2,'Tỉnh Hà Giang','Tỉnh'),(4,'Tỉnh Cao Bằng','Tỉnh'),(6,'Tỉnh Bắc Kạn','Tỉnh'),(8,'Tỉnh Tuyên Quang','Tỉnh'),(10,'Tỉnh Lào Cai','Tỉnh'),(11,'Tỉnh Điện Biên','Tỉnh'),(12,'Tỉnh Lai Châu','Tỉnh'),(14,'Tỉnh Sơn La','Tỉnh'),(15,'Tỉnh Yên Bái','Tỉnh'),(17,'Tỉnh Hoà Bình','Tỉnh'),(19,'Tỉnh Thái Nguyên','Tỉnh'),(20,'Tỉnh Lạng Sơn','Tỉnh'),(22,'Tỉnh Quảng Ninh','Tỉnh'),(24,'Tỉnh Bắc Giang','Tỉnh'),(25,'Tỉnh Phú Thọ','Tỉnh'),(26,'Tỉnh Vĩnh Phúc','Tỉnh'),(27,'Tỉnh Bắc Ninh','Tỉnh'),(30,'Tỉnh Hải Dương','Tỉnh'),(31,'Thành phố Hải Phòng','Thành phố Trung ương'),(33,'Tỉnh Hưng Yên','Tỉnh'),(34,'Tỉnh Thái Bình','Tỉnh'),(35,'Tỉnh Hà Nam','Tỉnh'),(36,'Tỉnh Nam Định','Tỉnh'),(37,'Tỉnh Ninh Bình','Tỉnh'),(38,'Tỉnh Thanh Hóa','Tỉnh'),(40,'Tỉnh Nghệ An','Tỉnh'),(42,'Tỉnh Hà Tĩnh','Tỉnh'),(44,'Tỉnh Quảng Bình','Tỉnh'),(45,'Tỉnh Quảng Trị','Tỉnh'),(46,'Tỉnh Thừa Thiên Huế','Tỉnh'),(48,'Thành phố Đà Nẵng','Thành phố Trung ương'),(49,'Tỉnh Quảng Nam','Tỉnh'),(51,'Tỉnh Quảng Ngãi','Tỉnh'),(52,'Tỉnh Bình Định','Tỉnh'),(54,'Tỉnh Phú Yên','Tỉnh'),(56,'Tỉnh Khánh Hòa','Tỉnh'),(58,'Tỉnh Ninh Thuận','Tỉnh'),(60,'Tỉnh Bình Thuận','Tỉnh'),(62,'Tỉnh Kon Tum','Tỉnh'),(64,'Tỉnh Gia Lai','Tỉnh'),(66,'Tỉnh Đắk Lắk','Tỉnh'),(67,'Tỉnh Đắk Nông','Tỉnh'),(68,'Tỉnh Lâm Đồng','Tỉnh'),(70,'Tỉnh Bình Phước','Tỉnh'),(72,'Tỉnh Tây Ninh','Tỉnh'),(74,'Tỉnh Bình Dương','Tỉnh'),(75,'Tỉnh Đồng Nai','Tỉnh'),(77,'Tỉnh Bà Rịa - Vũng Tàu','Tỉnh'),(79,'Thành phố Hồ Chí Minh','Thành phố Trung ương'),(80,'Tỉnh Long An','Tỉnh'),(82,'Tỉnh Tiền Giang','Tỉnh'),(83,'Tỉnh Bến Tre','Tỉnh'),(84,'Tỉnh Trà Vinh','Tỉnh'),(86,'Tỉnh Vĩnh Long','Tỉnh'),(87,'Tỉnh Đồng Tháp','Tỉnh'),(89,'Tỉnh An Giang','Tỉnh'),(91,'Tỉnh Kiên Giang','Tỉnh'),(92,'Thành phố Cần Thơ','Thành phố Trung ương'),(93,'Tỉnh Hậu Giang','Tỉnh'),(94,'Tỉnh Sóc Trăng','Tỉnh'),(95,'Tỉnh Bạc Liêu','Tỉnh'),(96,'Tỉnh Cà Mau','Tỉnh');
 /*!40000 ALTER TABLE `province` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `questions`
---
-
-DROP TABLE IF EXISTS `questions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `questions` (
-  `id` int NOT NULL,
-  `question` varchar(1000) DEFAULT NULL,
-  `quiz_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `quizidquestion_idx` (`quiz_id`),
-  CONSTRAINT `quizidquestion` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `questions`
---
-
-LOCK TABLES `questions` WRITE;
-/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `quiz_results`
---
-
-DROP TABLE IF EXISTS `quiz_results`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `quiz_results` (
-  `id` int NOT NULL,
-  `user_id` int DEFAULT NULL,
-  `quiz_id` int DEFAULT NULL,
-  `score` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `useridquizres_idx` (`user_id`),
-  KEY `quizidquizres_idx` (`quiz_id`),
-  CONSTRAINT `quizidquizres` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`),
-  CONSTRAINT `useridquizres` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `quiz_results`
---
-
-LOCK TABLES `quiz_results` WRITE;
-/*!40000 ALTER TABLE `quiz_results` DISABLE KEYS */;
-/*!40000 ALTER TABLE `quiz_results` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `quiz_skill`
---
-
-DROP TABLE IF EXISTS `quiz_skill`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `quiz_skill` (
-  `quiz_id` int NOT NULL,
-  `skill_id` int NOT NULL,
-  PRIMARY KEY (`quiz_id`,`skill_id`),
-  KEY `skillidquizskill_idx` (`skill_id`),
-  CONSTRAINT `quizidquizskill` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes` (`id`),
-  CONSTRAINT `skillidquizskill` FOREIGN KEY (`skill_id`) REFERENCES `skills` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `quiz_skill`
---
-
-LOCK TABLES `quiz_skill` WRITE;
-/*!40000 ALTER TABLE `quiz_skill` DISABLE KEYS */;
-/*!40000 ALTER TABLE `quiz_skill` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `quizzes`
---
-
-DROP TABLE IF EXISTS `quizzes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `quizzes` (
-  `id` int NOT NULL,
-  `name` varchar(450) DEFAULT NULL,
-  `mentor_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `mentoridquiz_idx` (`mentor_id`),
-  CONSTRAINT `mentoridquiz` FOREIGN KEY (`mentor_id`) REFERENCES `user_profiles` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `quizzes`
---
-
-LOCK TABLES `quizzes` WRITE;
-/*!40000 ALTER TABLE `quizzes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `quizzes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -796,4 +661,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-01 14:54:49
+-- Dump completed on 2022-06-02 15:36:46
