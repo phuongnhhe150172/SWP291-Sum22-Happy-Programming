e67fb8db7fa701b142c0c0059a0e3126f528d559
hoang nam <nam6626664266@gmail.com>
2022-06-08 17:05:32 +0700

    fix

--- src/main/java/swp/happyprogramming/services/servicesimpl/UserService.java
+++ src/main/java/swp/happyprogramming/services/servicesimpl/UserService.java
@@ -8,59 +8,168 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import swp.happyprogramming.repository.IUserRepository;
+import swp.happyprogramming.dto.ConnectionDTO;
 import swp.happyprogramming.dto.UserDTO;
 import swp.happyprogramming.exception.auth.UserAlreadyExistException;
-import swp.happyprogramming.model.Role;
-import swp.happyprogramming.model.User;
+import swp.happyprogramming.model.*;
+import swp.happyprogramming.repository.IAddressRepository;
+import swp.happyprogramming.repository.IMentorRepository;
+import swp.happyprogramming.repository.IUserRepository;
 import swp.happyprogramming.services.IUserService;
 
-import java.time.Instant;
-import java.util.Arrays;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class UserService implements IUserService {
-
-    private IUserRepository repository;
+    ModelMapper mapper = new ModelMapper();
 
     @Autowired
+    private IUserRepository userRepository;
+    @Autowired
+    private IAddressRepository addressRepository;
+    @Autowired
     private BCryptPasswordEncoder passwordEncoder;
+    @Autowired
+    private IMentorRepository mentorRepository;
 
-    public UserService(IUserRepository repository){
-        super();
-        this.repository = repository;
-    }
-
-    public User registerNewUserAccount(UserDTO userDTO) throws UserAlreadyExistException {
+    public void registerNewUserAccount(UserDTO userDTO) throws UserAlreadyExistException {
         if (emailExists(userDTO.getEmail())) {
             throw new UserAlreadyExistException("There is an account with that email address: " + userDTO.getEmail());
         }
-        ModelMapper mapper = new ModelMapper();
+        saveUser(userDTO);
+    }
+
+    private void saveUser(UserDTO userDTO) {
         userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
         User user = mapper.map(userDTO, User.class);
-        user.setCreated(Date.from(Instant.now()));
-        user.setModified(Date.from(Instant.now()));
-        user.setRoles(Arrays.asList(new Role("ROLE_USER")));
-        return repository.save(user);
+        Address address = new Address();
+        Address savedAddress = addressRepository.save(address);
+        user.setAddress(savedAddress);
+        user.addRole(new Role(2));
+        User savedUser = userRepository.save(user);
+        Mentor mentor = new Mentor();
+        mentor.setUser(savedUser);
+        mentorRepository.save(mentor);
     }
 
     private boolean emailExists(String email) {
-        return repository.findByEmail(email) != null;
+        return userRepository.findByEmail(email) != null;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = repository.findUserByEmail(username);
-        if (user == null){
+        User user = userRepository.findByEmail(username);
+        if (user == null) {
             throw new UsernameNotFoundException("User not found");
         }
-        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRoleToAuthorities(user.getRoles()));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                mapRoleToAuthorities(user.getRoles())
+        );
     }
 
-    private Collection<? extends GrantedAuthority> mapRoleToAuthorities(Collection<Role> roles){
-        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
+    @Override
+    public int countUsersByRolesLike(String role) {
+        return userRepository.countUsersByRolesLike("ROLE_MENTOR");
+    }
+
+    @Override
+    public int statusRequest(long mentorId, long menteeId) {
+        return userRepository.statusRequestByMentorIdAndMenteeId(mentorId, menteeId).orElse(-1);
+    }
+
+    private Collection<? extends GrantedAuthority> mapRoleToAuthorities(Collection<Role> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
+
+    public List<ConnectionDTO> getConnectionsByEmail(String email) {
+        ArrayList<User> users = userRepository.findConnectionsByEmail(email);
+        return users.stream()
+                .map(user -> new ConnectionDTO(
+                                user.getId(),
+                                user.getFirstName() + " " + user.getLastName()
+                        )
+                )
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        User user = userRepository.findByEmail(email);
+        Address address = addressRepository.findByAddressId(user.getAddress().getId());
+        user.setAddress(address);
+        return user;
+    }
+
+    @Override
+    public UserDTO findUser(long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) {
+            return null;
+        }
+        UserDTO userDTO = mapper.map(user, UserDTO.class);
+        return userDTO;
+    }
+
+    @Override
+    public UserDTO updateUserProfile(UserDTO userDTO, long wardId) {
+        User currentUser = userRepository.getById(userDTO.getId());
+        User user = mapper.map(userDTO, User.class);
+        user.setEmail(currentUser.getEmail());
+        user.setId(currentUser.getId());
+        user.setPassword(currentUser.getPassword());
+        user.getAddress().setId(currentUser.getAddress().getId());
+        user.setRoles(currentUser.getRoles());
+        userRepository.save(user);
+        updateAddress(userDTO, wardId);
+        return mapper.map(user, UserDTO.class);
+    }
+
+    @Override
+    public List<ConnectionDTO> getRequestsByEmail(String email) {
+        ArrayList<User> users = userRepository.findRequestsByEmail(email);
+        return users.stream()
+                .map(user -> new ConnectionDTO(
+                                user.getId(),
+                                user.getFirstName() + " " + user.getLastName()
+                        )
+                )
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<UserDTO> findAllMentees() {
+        List<User> mentees = userRepository.findUsersByRole("ROLE_MENTEE");
+        List<UserDTO> userDTOS = new ArrayList<>();
+        for(User mentee:mentees){
+            UserDTO userDTO = mapper.map(mentee, UserDTO.class);
+            userDTOS.add(userDTO);
+        }
+        return userDTOS;
+    }
+
+    @Override
+    public void removeMentee(long menteeId) {
+        User user = userRepository.getById(menteeId);
+        addressRepository.deleteById(user.getAddress().getId());
+        userRepository.deleteById(menteeId);
+    }
+
+    private void updateAddress(UserDTO userDTO, long wardId) {
+        Address address = addressRepository.findByAddressId(userDTO.getAddress().getId());
+        address.setName(userDTO.getAddress().getName());
+        Ward ward = new Ward();
+        ward.setId(wardId);
+        address.setWard(ward);
+        addressRepository.save(address);
     }
 }
