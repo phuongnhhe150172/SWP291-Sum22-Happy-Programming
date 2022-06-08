3125dd78f07403cfb08a582a5b1e9371c2e92886
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2022-06-08 17:35:56 +0700

    UPDATE user repo

--- src/main/java/swp/happyprogramming/dto/UserDTO.java
+++ src/main/java/swp/happyprogramming/dto/UserDTO.java
@@ -2,31 +2,57 @@ package swp.happyprogramming.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+import swp.happyprogramming.model.Address;
 import swp.happyprogramming.validator.auth.PasswordMatches;
 import swp.happyprogramming.validator.auth.ValidEmail;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.time.Instant;
+import java.util.Date;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
-@PasswordMatches
-@ValidEmail
+@PasswordMatches(message = "Passwords must match")
 public class UserDTO {
-    @NotNull
-    @NotEmpty
-    private String fullname;
+    private long id;
+    private long profileId;
+    private String firstName;
+    private String lastName;
+    private int gender;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date dob;
+    private String phoneNumber;
+    private Address address;
+    //    private String ward;
+//    private String district;
+//    private String province;
+    private String bio;
+    private String school;
+    private Double price;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date created;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date modified;
 
     @NotNull
     @NotEmpty
     private String password;
     private String matchingPassword;
 
+    @ValidEmail
     @NotNull
     @NotEmpty
     private String email;
+
+    @NotNull
+    private int role;
+    public UserDTO() {
+        this.created = Date.from(Instant.now());
+        this.modified = Date.from(Instant.now());
+    }
+
 }
--- src/main/java/swp/happyprogramming/repository/IUserRepository.java
+++ src/main/java/swp/happyprogramming/repository/IUserRepository.java
@@ -1,11 +1,32 @@
 package swp.happyprogramming.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import swp.happyprogramming.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String Email);
-    User findUserByEmail(String email);
+    User findByEmail(String email);
+
+    @Query(value = "select count(*) from user_roles where role_id in (select id from roles where `name` = ?1)", nativeQuery = true)
+    int countUsersByRolesLike(String role);
+
+    @Query(value = "select * from users where id in (select user_id from user_roles where role_id in (select id from roles where `name` = ?1))", nativeQuery = true)
+    List<User> findUsersByRole(String role);
+
+    @Query(value = "select r.status from request as r where r.mentor_id = ?1 and r.mentee_id = ?2", nativeQuery = true)
+    Optional<Integer> statusRequestByMentorIdAndMenteeId(long mentorId, long menteeId);
+
+    @Query(value = "select * from users where id in (select user1 from connections where user2 = (select id from users where email=?1) union select user2 from connections where user1 = (select id from users where email=?1))",
+            nativeQuery = true)
+    ArrayList<User> findConnectionsByEmail(String email);
+
+    @Query(value = "select * from users where id in (select mentor_id from request where mentee_id = (select id from users where email=?1) union select mentee_id from request where mentor_id = (select id from users where email=?1))",
+            nativeQuery = true)
+    ArrayList<User> findRequestsByEmail(String email);
 }
