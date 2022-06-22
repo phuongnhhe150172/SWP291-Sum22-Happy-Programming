d021c293dea27925245be0b1e8a5a2206f331fed
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2022-06-22 16:35:44 +0700

    REORDER config

--- src/main/java/swp/happyprogramming/config/AdditionalResourceWebConfiguration.java
+++ src/main/java/swp/happyprogramming/config/AdditionalResourceWebConfiguration.java
@@ -1,4 +1,4 @@
-package swp.happyprogramming.config.auth;
+package swp.happyprogramming.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
