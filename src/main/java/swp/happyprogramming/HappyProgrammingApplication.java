a892c90c7763dcc7bf604d588813b50ef168b4cb
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2022-06-24 08:30:31 +0700

    Update HappyProgrammingApplication.java

--- src/main/java/swp/happyprogramming/HappyProgrammingApplication.java
+++ src/main/java/swp/happyprogramming/HappyProgrammingApplication.java
@@ -2,12 +2,20 @@ package swp.happyprogramming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class HappyProgrammingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HappyProgrammingApplication.class, args);
+    }
+
+    @Configuration
+    @ComponentScan(lazyInit = true)
+    static class LocalConfig {
     }
 
 }
