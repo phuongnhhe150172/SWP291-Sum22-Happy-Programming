68d9783c15376a4eb7d31490429faa02f0cd8fb5
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2022-06-24 08:30:24 +0700

    Update ConnectionController.java

--- src/main/java/swp/happyprogramming/controllers/connection/ConnectionController.java
+++ src/main/java/swp/happyprogramming/controllers/connection/ConnectionController.java
@@ -1,11 +1,14 @@
 package swp.happyprogramming.controllers.connection;
 
+import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import swp.happyprogramming.dto.ConnectionDTO;
 import swp.happyprogramming.services.IUserService;
 
