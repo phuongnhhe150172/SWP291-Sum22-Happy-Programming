7acc6ef5025d052134c4857829f6acba5267fa3a
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2022-06-26 16:36:21 +0700

    CHANGE message pipeline

--- src/main/java/swp/happyprogramming/services/servicesimpl/MessageService.java
+++ src/main/java/swp/happyprogramming/services/servicesimpl/MessageService.java
@@ -2,10 +2,16 @@ package swp.happyprogramming.services.servicesimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import swp.happyprogramming.model.Message;
 import swp.happyprogramming.repository.IMessageRepository;
 import swp.happyprogramming.services.IMessageService;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
 import java.util.Comparator;
 import java.util.List;
 
@@ -27,5 +33,19 @@ public class MessageService implements IMessageService {
     @Override
     public void saveMessage(Message message) {
         messageRepository.save(message);
+    }
+
+    @Override
+    public String uploadImage(Long sender_id, Long receiver_id, Path currentFolder, MultipartFile image) {
+        Path imagesPath = Paths.get("src/main/upload/static/imgs");
+        String imageName = sender_id + "_" + receiver_id + "_" + Instant.now().getEpochSecond() + ".jpg";
+        Path imagePath = Paths.get(imageName);
+        Path file = currentFolder.resolve(imagesPath).resolve(imagePath);
+        try {
+            Files.write(file, image.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "/upload/static/imgs/" + imageName;
     }
 }
