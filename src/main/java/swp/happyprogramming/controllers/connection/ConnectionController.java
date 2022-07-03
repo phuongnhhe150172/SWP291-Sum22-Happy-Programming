c54cacf52e97fb5a7f5f9aad7ddcb40492cfa6b4
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2022-07-03 07:20:35 +0700

    UPDATE connection add profile link

--- src/main/java/swp/happyprogramming/controllers/connection/ConnectionController.java
+++ src/main/java/swp/happyprogramming/controllers/connection/ConnectionController.java
@@ -1,15 +1,9 @@
 package swp.happyprogramming.controllers.connection;
 
-import com.fasterxml.jackson.databind.DatabindContext;
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import swp.happyprogramming.dto.ConnectionDTO;
 import swp.happyprogramming.dto.UserDTO;
 import swp.happyprogramming.services.IUserService;
