bab0e6c645f4fe19e3fc8d106139d564fb36130e
dungle4320 <dungle4320@gmail.com>
2022-07-11 16:00:03 +0700

    interface for noti services

--- src/main/java/swp/happyprogramming/services/INotificationService.java
+++ src/main/java/swp/happyprogramming/services/INotificationService.java
@@ -9,4 +9,6 @@ import java.util.Set;
 
 public interface INotificationService {
     List<NotificationDTO> getNotificationByRoles(Set<Role> roles);
+    Notification save(Notification notification);
+    void informNotiForRole(long noti_id, long role_id);
 }
