9f9b7e93344d8917cf52ccdc6e98a237d33fd551
hoang nam <nam6626664266@gmail.com>
2022-07-18 08:51:17 +0700

    add update post

--- src/main/java/swp/happyprogramming/services/servicesimpl/PostService.java
+++ src/main/java/swp/happyprogramming/services/servicesimpl/PostService.java
@@ -66,6 +66,7 @@ public class PostService implements IPostService {
 
     private void updateInformationPost(PostDTO postDTO, long methodId, User user){
         Post postMap = mapper.map(postDTO, Post.class);
+        System.out.println(postMap.getUser().getId());
         postMap.setModified(Date.from(Instant.now()));
         Method method = methodRepository.findById(methodId);
         postMap.setMethod(method);
