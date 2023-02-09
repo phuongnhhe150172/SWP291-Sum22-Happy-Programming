fd800737ee63a49eaf793fb1295f4b95cfe200f0
prometheusalpha <hoangnguyen.hn212.hn@gmail.com>
2023-02-09 16:38:56 +0700

    feat(service): change service structure

--- src/main/java/swp/happyprogramming/application/services/UserService.java
+++ src/main/java/swp/happyprogramming/application/services/UserService.java
@@ -373,6 +373,158 @@ public class UserService implements IUserService {
     if (method == null) {
       return null;
     }
+<<<<<<< Updated upstream
     return mapper.map(method, MethodDTO.class);
   }
 }
+=======
+
+    public Pagination<UserAvatarDTO> getConnectionsById(long id, int pageNumber) {
+        PageRequest pageRequest = PageRequest.of(pageNumber - 1, 5);
+        Page<User> page = userRepository.findConnectionsById(pageRequest, id);
+        int totalPages = page.getTotalPages();
+        List<User> users = page.getContent();
+        List<UserAvatarDTO> connections = users.stream()
+                .map(user -> new UserAvatarDTO(
+                        user.getId(),
+                        user.getFirstName() + " " + user.getLastName(),
+                        user.getImage())
+                ).collect(Collectors.toList());
+        List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+        return new Pagination<>(connections, pageNumbers);
+    }
+
+    @Override
+    public List<UserAvatarDTO> getConnectionsById(long id) {
+        List<User> users = userRepository.findConnectionsById(id);
+        return users.stream()
+                .map(user -> new UserAvatarDTO(
+                        user.getId(),
+                        user.getFirstName() + " " + user.getLastName(),
+                        user.getImage())
+                ).collect(Collectors.toList());
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+
+    public UserDTO findUser(long id) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) return null;
+        return Utility.mapUser(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public UserDTO updateUserProfile(UserDTO userDTO) {
+        User currentUser = userRepository.getById(userDTO.getId());
+
+        Address address = currentUser.getAddress();
+
+        Ward ward = wardRepository
+                .findById(userDTO.getAddress().getWard().getId())
+                .orElse(null);
+
+        address.setWard(ward);
+        address.setName(userDTO.getAddress().getName());
+
+        // migrate info to the user
+        currentUser.setAddress(address);
+        currentUser.setFirstName(userDTO.getFirstName());
+        currentUser.setLastName(userDTO.getLastName());
+        currentUser.setBio(userDTO.getBio());
+        currentUser.setDob(userDTO.getDob());
+        currentUser.setGender(userDTO.getGender());
+        currentUser.setPhoneNumber(userDTO.getPhoneNumber());
+        currentUser.setSchool(userDTO.getSchool());
+        currentUser.setPrice(userDTO.getPrice());
+
+        userRepository.save(currentUser);
+        User userSaved = userRepository.findById(userDTO.getId()).orElse(new User());
+        return Utility.mapUser(userSaved);
+    }
+
+    @Override
+    public void removeMentee(long menteeId) {
+        User user = userRepository.getById(menteeId);
+        addressRepository.deleteById(user.getAddress().getId());
+        userRepository.deleteById(menteeId);
+    }
+
+    @Override
+    public void updateImage(Long id, Path currentFolder, MultipartFile image) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user == null) return;
+        Path imagesPath = Paths.get("src/main/upload/static/imgs");
+        String imageName = "image" + user.getId().toString() + ".jpg";
+        Path imagePath = Paths.get(imageName);
+        Path file = currentFolder.resolve(imagesPath).resolve(imagePath);
+        try {
+            Files.write(file, image.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String imageUrl = "/upload/static/imgs/" + imageName;
+        user.setImage(imageUrl);
+        userRepository.save(user);
+    }
+
+    @Override
+    public Pagination<UserDTO> getMentees(int pageNumber, String firstName, String lastName, String phone, String email) {
+        PageRequest pageRequest = PageRequest.of(pageNumber - 1, 10, Sort.Direction.DESC, "Id");
+        ArrayList<Role> roles = new ArrayList<>();
+        roles.add(new Role(2));
+        Specification<User> filtered = UserSpe.getUserSpe(firstName, lastName, phone, email, roles);
+        Page<User> page = userRepository.findAll(filtered, pageRequest);
+        int totalPages = page.getTotalPages();
+        List<User> mentees = page.getContent();
+        List<UserDTO> menteesDTO = mentees.stream()
+                .map(mentee -> findUser(mentee.getId()))
+                .collect(Collectors.toList());
+        List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages).boxed().collect(Collectors.toList());
+        return new Pagination<>(menteesDTO, pageNumbers);
+    }
+
+    public void updateResetPasswordToken(String token, String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user != null) {
+            user.setResetPasswordToken(token);
+            userRepository.save(user);
+        } else {
+            throw new UsernameNotFoundException("Could not find any user with the email " + email);
+        }
+    }
+
+    public User getByResetPasswordToken(String token) {
+        return userRepository.findByResetPasswordToken(token);
+    }
+
+    public void updatePassword(User user, String newPassword) {
+        BCryptPasswordEncoder password = new BCryptPasswordEncoder();
+        String encodedPassword = password.encode(newPassword);
+        user.setPassword(encodedPassword);
+        userRepository.save(user);
+    }
+
+    @Override
+    public void enableUser(long id) {
+        userRepository.enableUser(id);
+    }
+
+    @Override
+    public void disableUser(long id) {
+        userRepository.disableUser(id);
+    }
+
+    @Override
+    public List<Integer> getMonthlyNewMentees() {
+        return userRepository.getListAmountNewMentees();
+    }
+}
+>>>>>>> Stashed changes
